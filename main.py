dc2ed43e1c9c431a74c3ac2a0a528765a8f09b63
Francisco Lucas <lucasnasm@gmail.com>
2018-09-03 16:12:41 -0300

    Inclusão de try except para valor que não seja inteiro e parada de programa via control+C

--- main.py
+++ main.py
@@ -10,22 +10,31 @@ opcao=10
 
 
 while (FLAG_LOOP):
-  menuApresentacao()
-  opcao = int(input("Informe uma opção para iniciar: \n"))
-  if(opcao==0):
-    break
-  while(opcao!=0):      
-    if (opcao==1):
-      if(menuClientes()):
+  try:
+    menuApresentacao()
+    opcao = int(input("Informe uma opção para iniciar: \n"))
+  except KeyboardInterrupt:
+    print("")
+    print(" Ops! Para sair utilize a opção 0 ;)")
+    print("")
+  except ValueError:
+    print("")
+    print("Não entendi, digite uma das opções do menu para iniciar")
+    print("")
+  else:
+    if(opcao==0):
+      con.close()
+      break
+    while(opcao!=0):
+      if (opcao==1):
+        if(menuClientes()):
+          break
+      elif (opcao==2):
+        if(menuProdutos()):
+          break
+      elif (opcao==3):
+        if (menuVendas()):
+          break
+      else:
+        print(FLAG_MSG)
         break
-    elif (opcao==2):
-      if(menuProdutos()):
-        break
-    elif (opcao==3):
-      if (menuVendas()):
-        break
-    else:
-      print(FLAG_MSG)
-
- 
-   
