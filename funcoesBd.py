4793b2995d6bc180417d3c01c8f9f4e1af25ba9d
Francisco Lucas <lucasnasm@gmail.com>
2018-09-03 16:12:10 -0300

    Alteração dos espaçamentos do menu e funções.

--- funcoesBd.py
+++ funcoesBd.py
@@ -1,6 +1,7 @@
 # enconding: utf-8
 from db import *
 from pathlib import Path
+
 path_db = Path("pdv_mercado.db")
 if(path_db.exists()):
   con = sqlite3.connect("pdv_mercado.db")
@@ -17,30 +18,31 @@ def menuApresentacao():
   print("#"+"\033[31m"+"(3)"+"\033[0;0m"+" - Menu Vendas    \033[0;0m")
   print("#"+"\033[31m"+"(0)"+"\033[0;0m"+" - Sair   \033[0;0m")
 
+
 def menuClientes():
-  print("----------------------------------------------------------------------------------------------------------------")
+  print("-------------------------------------------------------------------------------------------------------------------")
   print("|\033[31m"+"(1)"+"\033[0;0m"+" - Listar Clientes | \033[31m"+"(2)"+"\033[0;0m"+" - Cadastrar Cliente | \033[31m"+"(3)"+"\033[0;0m"+" - Atualizar Cliente | \033[31m"+"(4)"+"\033[0;0m"+" - Remover Cliente | \033[31m"+"(0)"+"\033[0;0m"+" - Voltar |")
-  print("----------------------------------------------------------------------------------------------------------------")
+  print("-------------------------------------------------------------------------------------------------------------------")
 
   op_cliente = int(input("Informe uma opção para iniciar: \n"))
   if (op_cliente==1):
     listar("T_C")
   elif(op_cliente==2):
     inserir("T_C")
-  elif(op_cliente==3):    
+  elif(op_cliente==3):
     verificaId("T_C")
   elif(op_cliente==4):
     remover("T_C")
   elif(op_cliente==0):
-    return True 
+    return True
   else:
     print("Opção Invalida")
 
 
 def menuVendas():
-  print("----------------------------------------------------------------------------------------------------------------")
+  print("------------------------------------------------------------")
   print("|\033[31m"+"(1)"+"\033[0;0m"+" - Listar Vendas | \033[31m"+"(2)"+"\033[0;0m"+" - Realizar Venda | \033[31m"+"(0)"+"\033[0;0m"+"\033[0;0m"+" - Voltar |")
-  print("----------------------------------------------------------------------------------------------------------------")
+  print("------------------------------------------------------------")
 
   op_vendas= int(input("Informe uma opção para iniciar: \n"))
   if (op_vendas==1):
@@ -54,9 +56,9 @@ def menuVendas():
 
 
 def menuProdutos():
-  print("----------------------------------------------------------------------------------------------------------------")
+  print("-------------------------------------------------------------------------------------------------------------------")
   print("|"+"\033[31m"+"(1)"+"\033[0;0m"+" - Listar Produtos | \033[31m"+"(2)"+"\033[0;0m"+" - Cadastrar Produto | \033[31m"+"(3)"+"\033[0;0m"+" - Atualizar Produto | \033[31m"+"(4)"+"\033[0;0m"+" - Remover Produto | \033[31m"+"(0)"+"\033[0;0m"+" - Voltar |")
-  print("----------------------------------------------------------------------------------------------------------------")
+  print("-------------------------------------------------------------------------------------------------------------------")
   op_prod = int(input("Informe uma opção para iniciar: \n"))
   if (op_prod==1):
     listar("T_P")
@@ -67,10 +69,11 @@ def menuProdutos():
   elif(op_prod==4):
     remover("T_P")
   elif(op_prod==0):
-    return True 
+    return True
   else:
     print("Opção Invalida")
- 
+
+
 def inserir(seletor_menu):
     if (seletor_menu == "T_P"):
       cursor = con.cursor()
@@ -83,7 +86,6 @@ def inserir(seletor_menu):
       con.commit()
       cursor.close()
       print("Produto inserido com sucesso!!!")
-
     elif(seletor_menu == "T_C"):
       cursor = con.cursor()
       print("Digite as informações do cliente: ")
@@ -94,45 +96,35 @@ def inserir(seletor_menu):
       con.commit()
       cursor.close()
       print("Clinte inserido com sucesso!!!")
-
     else:
       print("Opção Desconhecida")
 
 
 def verificaId(seletor_menu):
   cursor = con.cursor()
-
   if(seletor_menu=="T_P"):
-
     listar(seletor_menu)
-
     id_prod = input("Informe o numero do produto: \n")
     cursor.execute("SELECT id FROM produto WHERE id=?", (id_prod,))
     consulta = cursor.fetchone()
-
     if not consulta:
         print("Produto não encontrado, tente novamente")
-
     else:
       listar(seletor_menu)
       atualizar(id_prod,seletor_menu)
-
   elif(seletor_menu=="T_C"):
     listar(seletor_menu)
     id_cli = input("Informe o numero de registro do cliente: \n")
     cursor.execute("SELECT id FROM cliente WHERE id=?", (id_cli,))
     consulta = cursor.fetchone()
-
     if not consulta:
       print("Cliente não encontrado, tente novamente")
-
     else:
       atualizar(id_cli,seletor_menu)
 
-def atualizar(id_reg,seletor_menu):
-  
-  cursor = con.cursor()
 
+def atualizar(id_reg,seletor_menu):
+  cursor = con.cursor()
   if (seletor_menu == "T_P"):
     print("Informe o novo nome do produto: ")
     descricao = input("Descrição: ")
@@ -142,7 +134,6 @@ def atualizar(id_reg,seletor_menu):
     con.commit()
     cursor.close()
     print("Produto atualizado com sucesso!!!")
-
   elif(seletor_menu == "T_C"):
     nome = input("Nome: ")
     telefone = input("Telefone: ")
@@ -150,13 +141,12 @@ def atualizar(id_reg,seletor_menu):
     con.commit()
     cursor.close()
     print("Cliente atualizado com sucesso!!!")
-
   else:
     print("Operação Invalida")
 
+
 def listar(seletor_menu):
   cursor = con.cursor()
-
   if (seletor_menu == "T_P"):
     sql_produto = "SELECT * FROM produto"
     cursor.execute(sql_produto)
@@ -181,27 +171,23 @@ def listar(seletor_menu):
       print("Telefone: ",registro[2])
       print("-------------")
     cursor.close()
-
   else:
     print("Opção Desconhecida")
 
+
 def remover(seletor_menu):
   listar(seletor_menu)
-
-  cursor = con.cursor() 
-
-  if (seletor_menu == "T_P"):    
+  cursor = con.cursor()
+  if (seletor_menu == "T_P"):
     id_remover = input("Informe o numero do produto que deseja remover: ")
     cursor.execute("DELETE FROM produto WHERE id=?",(id_remover))
     con.commit()
     cursor.close()
-
   elif (seletor_menu == "T_C"):
     id_remover = input("Informe o numero de registro do cliente que deseja remover: ")
     cursor.execute("DELETE FROM cliente WHERE id=?",(id_remover))
     con.commit()
     cursor.close()
-
   else:
     print("Função Invalida")
 
@@ -217,5 +203,3 @@ def vendas():
   con.commit()
   cursor.close()
   print("Venda realizada com sucesso")
-
-
