b2453ef5438f88b398d83c9d650319ef2a742892
Francisco Lucas <lucasnasm@gmail.com>
2018-09-03 16:11:16 -0300

    Alterações de espaçamento e identação.

--- db.py
+++ db.py
@@ -2,37 +2,33 @@
 #Importando a biblioteca SQLITE
 import sqlite3
 
-#Cria o cursor
-
 def criar_db():
   #Abrindo a conexao com o arquivo dados.db
   con = sqlite3.connect("pdv_mercado.db")
+  #Cria o cursor
   cursor = con.cursor()
   #Comando sql
   cursor.execute("""
-    CREATE TABLE produto(
-      id INTEGER PRIMARY KEY,
-      descricao VARCHAR(100),
-      valor NUMERIC(10,2),
-      quantidade INT
-    );
-  
+	CREATE TABLE produto(
+	  id INTEGER PRIMARY KEY,
+	  descricao VARCHAR(100),
+	  valor NUMERIC(10,2),
+	  quantidade INT
+	);
   """)
   cursor.execute("""
-    CREATE TABLE cliente(
-      id INTEGER PRIMARY KEY,
-      nome VARCHAR(100),
-      telefone VARCHAR(20)
-    );
+	CREATE TABLE cliente(
+	  id INTEGER PRIMARY KEY,
+	  nome VARCHAR(100),
+	  telefone VARCHAR(20)
+	);
   """)
-  
   cursor.execute("""
-    CREATE TABLE venda(
-      id INTEGER PRIMARY KEY,
-      id_cliente INT,
-      id_produto INT
-    );
+	CREATE TABLE venda(
+	  id INTEGER PRIMARY KEY,
+	  id_cliente INT,
+	  id_produto INT
+	);
   """)
-  
   #Fechando a conexao
   con.close()
